fe42fa207ed979a66d1bb1262ec17d3eecba81df
Anita Wu <43035019+Atturath@users.noreply.github.com>
2023-04-28 00:54:47 +1200

    Add files via upload
    # The main purpose is to get API every hour and store it in the cloud database (MongoDB)
    # There are four main parts: 1. Set up API 2. Set up database access 3. Main function to load str data into JSON and store it in the cloud(MongoDB) and 4. Schedule

---
+++ store_python.py
@@ -0,0 +1,60 @@
+# The main purpose is to get API every one hour and store it in the cloud database (MongoDB)
+# There are four main parts: 1.set up API, 2.set up database access, 3.main function to load str data into json and store it in cloud, and 4.schedule
+
+import json
+import requests
+from pymongo import MongoClient
+import schedule
+import time
+
+#-------------API-----------#
+key = '6eed86a9a0984545924b74935e335283'
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key
+}
+
+url = 'https://api.at.govt.nz/realtime/legacy'
+
+#-------------MongoDB-----------#
+client = MongoClient("mongodb+srv://Anita:Y6cF7ICXqbZyFgwL@cluster0.7gfdq86.mongodb.net/test")
+db = client.get_database('AT_Bus')
+records = db.AT_Bus
+
+#-------------Data Structure-----------#
+
+
+#-------------Main Function-----------#
+def Bus_data():
+    # Make the API request
+    response = requests.get(url,headers=headers)
+
+    print(response.status_code)
+
+    # Check the response status code
+    if response.status_code == 200:
+        # Request was successful
+    
+        json_data = json.loads(response.text)["response"]
+
+    else:
+        # Request encountered an error
+        print(f"API request failed with status code {response.status_code}")
+    
+    records.insert_one(json_data)
+
+#-------------Timed task-----------#    
+schedule.every(1).hour.do(Bus_data)
+
+count = 0
+while count<24*5:
+    schedule.run_pending()
+    time.sleep(3600)
+    print(count)
+    count+=1
+    
+
+
+
+
+
